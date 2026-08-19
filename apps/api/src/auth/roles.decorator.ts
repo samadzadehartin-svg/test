18c5c1d4d13504cb8a057ae0cfa2490fa5c5869c
samadzadehartin-svg <samadzadehartin@gmail.com>
2026-08-19 15:20:18 +0330

    A

--- apps/api/src/auth/roles.decorator.ts
+++
@@ -1,5 +0,0 @@
-import { SetMetadata } from "@nestjs/common";
-import type { StaffRole } from "./auth.types";
-
-export const ROLES_KEY = "staff_roles";
-export const Roles = (...roles: StaffRole[]) => SetMetadata(ROLES_KEY, roles);
